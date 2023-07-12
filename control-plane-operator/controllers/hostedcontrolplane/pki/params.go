6179ade081e4550532c65910cbbc9f76aea62cb5
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2023-07-12 13:13:15 +0200

    OCPBUGS-16049: KAS Certificates now includes the proper SAN for dual stack scenarios
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- control-plane-operator/controllers/hostedcontrolplane/pki/params.go
+++ control-plane-operator/controllers/hostedcontrolplane/pki/params.go
@@ -95,5 +95,6 @@ func NewPKIParams(hcp *hyperv1.HostedControlPlane,
 	} else {
 		p.NodeInternalAPIServerIP = util.AdvertiseAddressWithDefault(hcp, config.DefaultAdvertiseIPv6Address)
 	}
+
 	return p
 }
